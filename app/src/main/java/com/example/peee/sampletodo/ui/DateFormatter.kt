0eaa649506d18c6239cd3cd724678c4a5fb7ff09
Jumpei Togashi <jumpei.togashi@gmail.com>
2017-11-30 00:00:04 +0900

    Add DateFormatter to parse date string and time millis

---
+++ app/src/main/java/com/example/peee/sampletodo/ui/DateFormatter.kt
@@ -0,0 +1,21 @@
+package com.example.peee.sampletodo.ui
+
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
+
+object DateFormatter {
+    private const val PATTERN = "YYYY/MM/dd HH:mm"
+    private val format = SimpleDateFormat(PATTERN, Locale.US)
+
+    fun toString(time: Long): String = format.format(Date(time))
+
+    fun toMillis(dateFormat: String): Long {
+        try {
+            return format.parse(dateFormat).time
+        } catch (e: ParseException) {
+            e.printStackTrace()
+            return Date(0).time
+        }
+    }
+}
