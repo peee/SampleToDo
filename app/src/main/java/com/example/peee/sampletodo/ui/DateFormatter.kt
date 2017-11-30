70be0828125787221344dd2aaa5cabe092809148
Jumpei Togashi <jumpei.togashi@gmail.com>
2017-12-01 00:14:19 +0900

    DateFormatter: Correct format pattern

--- app/src/main/java/com/example/peee/sampletodo/ui/DateFormatter.kt
+++ app/src/main/java/com/example/peee/sampletodo/ui/DateFormatter.kt
@@ -5,7 +5,7 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 object DateFormatter {
-    private const val PATTERN = "YYYY/MM/dd HH:mm"
+    private const val PATTERN = "yyyy/MM/dd HH:mm"
     private val format = SimpleDateFormat(PATTERN, Locale.US)
 
     fun toString(time: Long): String = format.format(Date(time))
