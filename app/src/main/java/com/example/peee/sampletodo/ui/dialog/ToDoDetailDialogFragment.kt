9d8d29e0484af779c9d50d1464217fc6d03ad797
Jumpei Togashi <jumpei.togashi@gmail.com>
2017-12-02 22:44:59 +0900

    ToDoDetailDialogFragment: Switch enable status of due date views

--- app/src/main/java/com/example/peee/sampletodo/ui/dialog/ToDoDetailDialogFragment.kt
+++ app/src/main/java/com/example/peee/sampletodo/ui/dialog/ToDoDetailDialogFragment.kt
@@ -16,16 +16,32 @@ class ToDoDetailDialogFragment : DialogFragment(),
         DatePickerDialog.OnDateSetListener,
         TimePickerDialog.OnTimeSetListener {
 
+    private lateinit var switch: Switch
+    private lateinit var date: TextView
+    private lateinit var time: TextView
+    private lateinit var dateSetter: Button
+    private lateinit var timeSetter: Button
+
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val view = View.inflate(activity, R.layout.dialog_todo_detail, null)
 
         with (view) {
-            findViewById<Button>(R.id.dialog_button_todo_item_due_date).setOnClickListener {
-                DatePickerDialogFragment().show(childFragmentManager, "date_picker_due_date")
-            }
-            findViewById<Button>(R.id.dialog_button_todo_item_due_date_time).setOnClickListener {
-                TimePickerDialogFragment().show(childFragmentManager, "time_picker_due_date")
-            }
+            switch = findViewById(R.id.dialog_switch_todo_item_due_date)
+            date = findViewById(R.id.dialog_text_todo_item_due_date)
+            time = findViewById(R.id.dialog_text_todo_item_due_date_time)
+            dateSetter = findViewById(R.id.dialog_button_todo_item_due_date)
+            timeSetter = findViewById(R.id.dialog_button_todo_item_due_date_time)
+        }
+
+        refreshEnableState(switch.isChecked)
+        switch.setOnCheckedChangeListener {
+            _, isChecked -> refreshEnableState(isChecked)
+        }
+        dateSetter.setOnClickListener {
+            DatePickerDialogFragment().show(childFragmentManager, "date_picker_due_date")
+        }
+        timeSetter.setOnClickListener {
+            TimePickerDialogFragment().show(childFragmentManager, "time_picker_due_date")
         }
 
         return AlertDialog.Builder(activity)
@@ -40,6 +56,23 @@ class ToDoDetailDialogFragment : DialogFragment(),
                 .create()
     }
 
+    private fun refreshEnableState(isChecked: Boolean) {
+        date.isEnabled = isChecked
+        time.isEnabled = isChecked
+        dateSetter.isEnabled = isChecked
+        timeSetter.isEnabled = isChecked
+
+        if (isChecked) {
+            date.text = DateFormatter.toDateString(System.currentTimeMillis())
+            time.text = DateFormatter.toTimeString(System.currentTimeMillis())
+            dateSetter.setOnClickListener {
+                DatePickerDialogFragment().show(childFragmentManager, "date_picker_due_date")
+            }
+            timeSetter.setOnClickListener {
+                TimePickerDialogFragment().show(childFragmentManager, "time_picker_due_date")
+            }
+        }
+    }
 
     private fun buildToDo(view: View) {
         val title = view.findViewById<EditText>(R.id.dialog_edit_todo_item_title).text.toString()
