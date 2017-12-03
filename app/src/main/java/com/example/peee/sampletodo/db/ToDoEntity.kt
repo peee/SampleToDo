bb70e1b14d3b24614ec6b0f88d931d487b0ec260
Jumpei Togashi <jumpei.togashi@gmail.com>
2017-12-03 17:52:45 +0900

    ToDoEntity: Set Serializable

--- app/src/main/java/com/example/peee/sampletodo/db/ToDoEntity.kt
+++ app/src/main/java/com/example/peee/sampletodo/db/ToDoEntity.kt
@@ -2,11 +2,12 @@ package com.example.peee.sampletodo.db
 
 import android.arch.persistence.room.Entity
 import android.arch.persistence.room.PrimaryKey
+import java.io.Serializable
 
 @Entity(tableName = "todo")
 data class ToDoEntity(var title: String = "",
                  var description: String = "",
                  var dueDate: Long = 0,
-                 var reminder: Long = 0) {
+                 var reminder: Long = 0) : Serializable {
     @PrimaryKey(autoGenerate = true) var id: Long = 0
 }
