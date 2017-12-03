c4ce40aa5a68f1e51aaeae72a00915f5b3321f96
Jumpei Togashi <jumpei.togashi@gmail.com>
2017-12-03 23:55:50 +0900

    db: Add KDoc

--- app/src/main/java/com/example/peee/sampletodo/db/ToDoDao.kt
+++ app/src/main/java/com/example/peee/sampletodo/db/ToDoDao.kt
@@ -2,17 +2,42 @@ package com.example.peee.sampletodo.db
 
 import android.arch.persistence.room.*
 
+/**
+ * A DAO: Data Access Object for [ToDoEntity] and [ToDoDatabase] using the Room
+ */
 @Dao
 interface ToDoDao {
+    /**
+     * Loads all to-do items from the DB.
+     *
+     * @return list of to-do items in the DB
+     */
     @Query("SELECT * FROM todo")
     fun loadAllItems(): List<ToDoEntity>
 
+    /**
+     * Loads a to-do item that matches given ID.
+     *
+     * @param id id of to-do item in the DB
+     * @return to-do item matched
+     */
     @Query("SELECT * FROM todo where id = :arg0")
     fun loadItem(id: Long): ToDoEntity
 
+    /**
+     * Inserts given to-do item in the DB if its ID doesn't exist,
+     * or updates if exists
+     *
+     * @param todo to-do item to insert or update
+     */
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertOrUpdate(todo: ToDoEntity)
 
+    /**
+     * Deletes given to-do item in the DB.
+     *
+     * @param todo to-do item to delete
+     */
     @Delete
     fun delete(todo: ToDoEntity)
 }
--- app/src/main/java/com/example/peee/sampletodo/db/ToDoDatabase.kt
+++ app/src/main/java/com/example/peee/sampletodo/db/ToDoDatabase.kt
@@ -5,15 +5,27 @@ import android.arch.persistence.room.Room
 import android.arch.persistence.room.RoomDatabase
 import android.content.Context
 
+/**
+ * A Room DB that holds [ToDoEntity], accessed by [ToDoDao].
+ * [getInstance] has to be used to get the instance.
+ */
 @Database(entities = arrayOf(ToDoEntity::class), version = 1)
 abstract class ToDoDatabase : RoomDatabase() {
 
+    /**
+     * Overridden by Room annotation processing.
+     */
     abstract fun todoDao(): ToDoDao
 
     companion object {
         private const val DB_NAME = "todo.db"
         private var instance: ToDoDatabase? = null
 
+        /**
+         * Gets the single instance of [ToDoDatabase].
+         *
+         * @param context context to build Room database
+         */
         fun getInstance(context: Context) = instance ?: synchronized(this) {
             return buildDatabase(context).also{ instance = it }
         }
