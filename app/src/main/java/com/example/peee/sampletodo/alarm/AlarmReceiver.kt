88dda8f25c3297a9114611a24038ae55c4d0771c
Jumpei Togashi <jumpei.togashi@gmail.com>
2017-12-03 22:30:32 +0900

    AlarmReceiver: Post to-do reminder notification

--- app/src/main/java/com/example/peee/sampletodo/alarm/AlarmReceiver.kt
+++ app/src/main/java/com/example/peee/sampletodo/alarm/AlarmReceiver.kt
@@ -3,10 +3,14 @@ package com.example.peee.sampletodo.alarm
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import com.example.peee.sampletodo.notification.ReminderNotification
 
 class AlarmReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
         context ?: return
         intent ?: return
+
+        val title = intent.getStringExtra(AlarmHelper.EXTRA_TODO)
+        ReminderNotification.post(context, title)
     }
 }
